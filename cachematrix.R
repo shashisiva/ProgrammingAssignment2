f05d658593b181d14185c7fb71d7f9945a33a34e
Sivapragasam <Shashikala.Sivapragasam@CAPCO.COM>
2017-10-31 07:20:33 -0400

    Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below creates an inverse of a matrix and caches the data
+## The functions are makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+## This function returns a function that sets and gets the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) m <<- solve
+    getInverse <- function() m
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of a matrix if its not already computed
+## If the inverse is already computed (from previous execution), then the inverse
+## is returned from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
+
+# Execution and sample working. You can replace A with any matrix
+A = matrix( c(1,2,3,0,1,4,5,6,0), nrow=3, ncol=3, byrow = TRUE)
+A
+matA <- makeCacheMatrix(A)
+matA
+cacheSolve(matA)
+# Note the second time cacheSolve is called the data should be retrieved from
+# the cache instead of computing the inverse
+cacheSolve(matA)
